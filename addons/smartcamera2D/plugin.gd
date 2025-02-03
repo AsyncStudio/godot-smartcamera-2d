6cac5c37449edf67ce34c4396655b3fe5fed19ad
AndreMicheletti <andre.micheletti@gazeus.com>
2025-02-03 15:39:14 -0300

    feat: add icon

--- addons/smartcamera2D/plugin.gd
+++ addons/smartcamera2D/plugin.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	# Add the new type with a name, a parent type, a script and an icon.
-	add_custom_type("SmartCamera2D", "Camera2D", preload("SmartCamera2D.gd"), null)
+	add_custom_type("SmartCamera2D", "Camera2D", preload("SmartCamera2D.gd"), preload("Camera2D.svg"))
 	add_autoload_singleton("CameraControl", "CameraControl.gd")
 
 func _exit_tree():
